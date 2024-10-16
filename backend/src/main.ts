1b08e41f3b8d9299d4809ff7b96daedcb1d848ce
Wei Ji (FlySkyPie) <c445dj544@gmail.com>
2024-10-16 21:23:01 +0800

    feat: Add zeromq communication.

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,1 +1,22 @@
-console.log("OHO")
+import { Request } from "zeromq";
+
+async function run() {
+    const sock = new Request();
+
+    sock.connect("tcp://127.0.0.1:38989");
+    console.log("Producer bound to port 38989");
+
+    // Simulate game loop.
+    setInterval(async () => {
+        try {
+            await sock.send("4");
+            const [result] = await sock.receive();
+
+            console.log("Server got result:", result);
+        } catch (error) {
+            console.error(error);
+        }
+    }, 100);
+}
+
+run()
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,8 +1,13 @@
+import { Reply, Context } from "zeromq";
+
 import GamePad from '../libs/node-gamepad';
 
 const controller = new GamePad('logitech/gamepadf310');
 // const controller = new GamePad('logitech/gamepadf310', { debug: true });
 controller.connect();
+
+const controlBuffer = new ArrayBuffer(8);
+const view = new Uint8Array(controlBuffer);
 
 controller.on('dpadUp:press', () => {
     console.log('up');
@@ -12,9 +17,30 @@ controller.on('dpadDown:press', () => {
 });
 
 controller.on('left:move', (s) => {
-    console.log('left', s);
+    view[0] = s.x;
+    view[1] = s.y;
+    // console.log('left', s);
 });
 
 controller.on('right:move', (s) => {
-    console.log('right', s);
+    view[2] = s.x;
+    view[3] = s.y;
+    // console.log('right', s);
 });
+
+async function run() {
+    const context = new Context({ maxSockets: 1 });
+    const sock = new Reply({ context });
+
+    await sock.bind("tcp://127.0.0.1:38989");
+
+    for await (const [inputData] of sock) {
+        // console.log(inputData)
+
+        // Do inference (by AI) here
+
+        await sock.send(controlBuffer);
+    }
+}
+
+run();
